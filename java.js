cae354006665f77978ebdb9fdc3537a04bd59023
pankaj <126156801+paieageles@users.noreply.github.com>
2023-02-23 15:44:06 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+function fn(){
+    var a= parseInt(document.getElementById('num1').value);
+    var b= parseInt(document.getElementById('num2').value);
+document.getElementById('res').innerHTML=a-a*b/100;
+}
